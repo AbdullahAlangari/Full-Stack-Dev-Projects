f85848186deaf3c60eca53709142f679fc10a12e
klorex1281 <abodov2011@hotmail.com>
2021-05-29 19:48:04 +0300

    Final Submission - Edited for resubmission

--- projects/01_fyyur/starter_code/config.py
+++ projects/01_fyyur/starter_code/config.py
@@ -8,6 +8,11 @@ DEBUG = True
 
 # Connect to the database
 
+#app.config['DEBUG'] = True
+#app.config[''] =False
+
 
 # TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = '<Put your local database url>'
+SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:abodov1281@localhost:5432/fyyur'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+
